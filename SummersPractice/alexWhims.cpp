38795db45982af44767c40857cbcfbe190283658
Somyajeet <isomya13@gmail.com>
2024-06-02 13:17:05 +0530

    very easy

---
+++ SummersPractice/alexWhims.cpp
@@ -0,0 +1,66 @@
+/*
+problem link ->
+https://codeforces.com/problemset/problem/1899/F
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define loop(i,l,r)     for(int i=l; i<r; i++)
+#define int             long long
+#define pb              push_back
+#define vi              vector<int>
+#define mkp             make_pair<int,int>
+#define umpii           unordered_map<int,int>
+#define maxheap         priority_queue<int>
+#define minheap         priority_queue<int, vi,greater<int>>
+#define setbits(x)      __builtin_popcountll(x)
+#define zerobits(x)     __builtin_ctzll(x)
+#define in_arr(A,n)     loop(i,0,n) cin>>A[i];
+#define p_arr(A,n)      loop(i,0,n) cout<<A[i]
+;
+#define pln_arr(A,n)    loop(i,0,n) cout<<A[i]<<endl
+#define take_n          int n; cin>>n;
+#define take_arr        int arr[n]; loop(i,0,n) cin>>arr[i];
+  
+  
+const int mod= 1e9+7;
+const int inf= 1e15;
+ 
+ 
+void solve(){
+    int n,q; cin>>n>>q;
+
+    vi d(q);
+    loop(i,0,q) cin>>d[i];
+
+    for(int i=0; i<n-1; i++){
+        cout<<i+1<<" "<<i+2<<endl;
+    }
+
+    int par_of_1 = 2;
+
+    for(int i=0; i<q; i++){
+        int req = d[i];
+
+        /*
+        1-2-3-4-5
+        */
+
+       int new_par = req+1;
+       if(par_of_1 == new_par){
+        cout<<-1<<" "<<-1<<" "<<-1<<endl;
+       }
+       else{
+        cout<<1<<" "<<par_of_1<<" "<<new_par<<endl;
+        par_of_1 = new_par;
+       }
+    }
+}
+  
+  
+int32_t main(){
+int t;
+cin>>t;
+while(t--) solve();
+return 0;
+}
